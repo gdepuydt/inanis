09868a77cb3af2f217747e2df5b2e5ca0c6faf38
gdepuydt <geert_depuydt@hotmail.com>
2018-09-25 22:12:50 +0200

    lexer + uility functions for tokens

--- ionide_compiler/ionide_compiler/ionide.c
+++ ionide_compiler/ionide_compiler/ionide.c
@@ -6,6 +6,9 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
 
 #define MAX(x,y) ((x) >= (y) ? (x) : (y))
 
@@ -29,6 +32,19 @@ void *xmalloc(size_t num_bytes) {
 	return ptr;
 }
 
+/*UTILITY FUNCTIONS*/
+
+//fatal function prints fatal error messages
+void fatal(const char *fmt, ...) {
+	va_list args;
+	va_start (args, fmt);
+	printf("FATAL: ");
+	vprintf(fmt, args);
+	printf("\n");
+	va_end(args);
+	exit(1);
+}
+
 /*DYNAMIC ARRAYS aka STRETCHY BUFFERS*/
 
 typedef struct BufHdr {
@@ -43,7 +59,7 @@ typedef struct BufHdr {
 
 #define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
 #define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
-#define buf_push(b,x) (buf__fit(b, 1), (b)[buf_len(b)] = (x), buf__hdr(b)->len++)
+#define buf_push(b,x) (buf__fit(b, 1), (b)[buf__hdr(b)->len++] = (x))
 #define buf_free(b) ((b) ? free(buf__hdr(b)), (b) = NULL : 0)
 
 void *buf__grow(const void *buf, int new_len, size_t elem_size) {
@@ -66,7 +82,7 @@ void *buf__grow(const void *buf, int new_len, size_t elem_size) {
 	return new_hdr->buf;
 }
 
-int buf_test() {
+bool buf_test() {
 	
 	int *lala = NULL;
 	buf_push(lala, 42);
@@ -75,7 +91,7 @@ int buf_test() {
 		printf("%d\n", lala[i]);
 	}
 	buf_free(lala);
-	return 1;
+	return true;
 }
 
 /*STRING INTERNING*/
@@ -97,15 +113,26 @@ const char *str_intern_range(const char *start, const char *end) {
 		}
 	}
 	
-	char *str = malloc(len + 1);
+	char *str = xmalloc(len + 1); //so the strings will be randomly(?) stored in memory, but the pointer to these locations will be kept in the InternStr struct as a dynamic array.
 	memcpy(str, start, len);
 	str[len] = 0;
-	InternStr new_intern = (InternStr) { len, str };
-	buf_push(interns, new_intern);
+	buf_push(interns, ((InternStr) { len, str }));
+	return str;
 }
 
+//this one is for 0-terminated string interning
 const char *str_intern(const char *str) {
+	return str_intern_range(str, str + strlen(str));
+}
 
+bool str_intern_test() {
+	char x[] = "hello";
+	char y[] = "hello";
+
+	assert(x != y);
+	assert(str_intern(x) == str_intern(y));
+
+	return true;
 }
 
 
@@ -124,8 +151,32 @@ typedef struct Token {
 	const char *end;
 	union {
 		uint64_t val;
+		char *name;
 	};
 }Token;
+
+//Warning: This returns a pointer to a static internal buffer that will be overwritten next time!
+const char *token_kind_name(TokenKind kind) {
+	static char buf[256];
+	switch (kind) {
+	case TOKEN_INT:
+		sprintf(buf,"integer");
+		break;
+	case TOKEN_NAME:
+		sprintf(buf, "name");
+		break;
+	default:
+		if (kind < 128 && isprint(kind)) {
+			sprintf(buf, "%c", kind);
+			break;
+		}
+		else {
+			sprintf(buf, "ASCII: %d", kind);
+		}
+	}
+	return buf;
+}
+
 
 const char *stream;
 Token token;
@@ -210,6 +261,7 @@ void next_token() {
 				stream++;
 			}
 			token.kind = TOKEN_NAME;
+			token.name = str_intern_range(token.start, stream);
 			break;
 		}
 		default:
@@ -233,8 +285,8 @@ void print_token(Token token) {
 	}
 }
 
-int lex_test() {
-	char *source = "+-[]_HELLO12,3456FOO!789s";
+bool lex_test() {
+	char *source = "XY+-[]XY,_HELLO12,3456FOO!789s";
 	stream = source;
 	next_token();
 	while (token.kind) {
@@ -242,8 +294,47 @@ int lex_test() {
 		print_token(token);
 		next_token();
 	}
+	return true;
 }
 
+
+//check if the current token is expected, without consuming the next token.
+inline bool is_token(TokenKind kind) {
+	return token.kind == kind;
+}
+
+//does the current token name equal a given name?
+inline bool is_token_name(const char *name) {
+	return token.kind == TOKEN_NAME && token.name == name;
+}
+
+//the current token matches a given token kind?
+inline bool match_token(TokenKind kind) {
+	if (is_token(kind)) {
+		next_token();
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//Fatal error when the current token is not as expected
+inline bool expect_token(TokenKind kind) {
+	if (is_token(kind)) {
+		next_token();
+		return true;
+	}
+	else {
+		fatal("Expected token %s , but got %s\n", token_kind_name(kind), token_kind_name(token.kind));
+	}
+}
+
+/*TODO: use the above token functions to write a simple expression parser*/
+
+void parse_expr() {
+	//..
+}
 
 int main(int argc, char **argv) {
 	for (int i = 0; i < argc; i++) {
@@ -254,6 +345,9 @@ int main(int argc, char **argv) {
 	}
 	if (lex_test()) {
 		printf("passed lex_test\n");
+	}
+	if (str_intern_test()) {
+		printf("passed str_intern_test\n");
 	}
 	return 0;
 }
